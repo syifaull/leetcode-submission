43b01c3ffbfbe3a466787ab2cec67b0e45772fd5
unknown <syifaulya0610@gmail.com>
2022-10-23 23:18:15 +0700

    solving running sum of 1d array

---
+++ running-sum-of-1d-array.js
@@ -0,0 +1,9 @@
+var runningSum = function(nums) {
+  let newArray = [nums[0]];
+  for (let i = 0; i < nums.length; i ++) {
+    newArray.push(nums[i] + newArray[newArray.length - 1])
+  } 
+  return newArray;
+};
+
+console.log(runningSum([1, 2, 3, 4]));
